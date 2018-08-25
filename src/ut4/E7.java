11e04abb23b6545fb14fed82e182048828ef036d
Daasty <daastiiy@gmail.com>
2018-08-25 21:21:18 +0200

    E6 medio hecho y E7 sin hacer

---
+++ src/ut4/E7.java
@@ -0,0 +1,9 @@
+package ut4;
+/*
+Modificación del anterior que muestre de resultado la primera posición del vector con el valor y el número de posiciones
+ */
+public class E7 {
+    public static void main(String[] args) {
+        
+    }
+}
